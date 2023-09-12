c7a4ed503064060b4527feaf4c5d8570b077244e
unknown <martindapol@gmail.com>
2023-09-12 20:30:48 -0300

    Pilas y Colas parte I

---
+++ PilasColas/implementaciones/Pila.cs
@@ -0,0 +1,50 @@
+﻿using PilasColas.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilasColas.implementaciones
+{
+    public class Pila : IColeccion
+    {
+        private List<object> elementos;
+
+        public Pila()
+        {
+            elementos = new List<object>();
+        }
+        public bool Agregar(object obj)
+        {
+            elementos.Insert(0, obj);
+            return true;
+        }
+
+        public bool EstaVacia()
+        {
+            return elementos.Count == 0;
+        }
+
+        public object Extraer()
+        {
+            object e = null;
+
+            if (!EstaVacia())
+            {
+                e = elementos[0];
+                elementos.RemoveAt(0);
+            }
+            return e;
+        }
+
+        public object Primero()
+        {
+            object e = null;
+            if (!EstaVacia())
+                e = elementos[0];
+            return e;
+        }
+
+    }
+}
---
+++ PilasColas/interfaces/ICollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilasColas.interfaces
+{
+    public interface IColeccion
+    {
+        bool EstaVacia();
+        object Extraer();
+        object Primero();
+        bool Agregar(object obj);
+
+    }
+}
